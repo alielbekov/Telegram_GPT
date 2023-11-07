75e73ba20c7f8287e79beba4a9e5f7a1db92a3dc
alielbekov <eldorbek2001@arizona.edu>
2023-11-06 20:41:38 -0700

    setup voice to text

--- server.js
+++ server.js
@@ -1,70 +1,64 @@
-require('dotenv').config(); // make sure to install the dotenv package
-
-const {Telegraf} = require('telegraf');
+require('dotenv').config();
+const { Telegraf } = require('telegraf');
 const speech = require('@google-cloud/speech');
-const openAI = require("openai");
 const fs = require('fs');
 const axios = require('axios');
-const ffmpegInstaller = require('@ffmpeg-installer/ffmpeg');
 const ffmpeg = require('fluent-ffmpeg');
+const ffmpegInstaller = require('@ffmpeg-installer/ffmpeg');
 ffmpeg.setFfmpegPath(ffmpegInstaller.path);
-// path to your JSON key file
-const keyFilename = process.env.TEXT_TO_SPEECH_FILE_PATH;
+
+require('dotenv').config(); // make sure to install the dotenv package
+
+const keyFilename = process.env.TEXT_TO_SPEECH_FILE_PATH; // path to your JSON key file
 
 // Creates a client
-const client = new speech.SpeechClient({ keyFilename });
+const googleClient = new speech.SpeechClient({ keyFilename });
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+// Helper function to download the voice message and convert it to WAV
+async function downloadAndConvertVoiceToWAV(fileUrl, originalPath, outputPath) {
+  const writer = fs.createWriteStream(originalPath);
+  const response = await axios({
+    url: fileUrl,
+    responseType: 'stream',
+  });
 
+  response.data.pipe(writer);
 
-  // Helper function to convert voice messages to WAV
-async function convertVoiceToWAV(voiceUrl) {
-    // Download the file from the voice URL
-    return axios({
-      url: voiceUrl,
-      responseType: 'stream', // Notice the responseType is 'stream'
-    }).then(response => {
-      // Promise wrapper around the conversion process
-      return new Promise((resolve, reject) => {
-        // Define the output file name
-        const outputFilename = './output.wav';
-        // Create a write stream for the output file
-        const outputStream = fs.createWriteStream(outputFilename);
-        // Set up the ffmpeg command
-        ffmpeg(response.data) // Passing the response stream directly to ffmpeg
-          .inputFormat('ogg')
-          .audioCodec('pcm_s16le') // Linear16 for Google Speech-to-Text
-          .audioFrequency(48000)
-          .on('error', (err) => {
-            // Handle ffmpeg error
-            console.error('An error occurred during the conversion process:', err);
-            reject(err);
-          })
-          .on('end', () => {
-            // Conversion finished
-            console.log('Audio conversion finished.');
-            // Read the converted WAV file
-            const wavBuffer = fs.readFileSync(outputFilename);
-            resolve(wavBuffer);
-            // Clean up the WAV file after reading it
-            fs.unlinkSync(outputFilename);
-          })
-          // Direct the output to the write stream
-          .pipe(outputStream, { end: true });
-      });
+  return new Promise((resolve, reject) => {
+    writer.on('finish', () => {
+      ffmpeg(originalPath)
+        .outputFormat('wav')
+        .audioCodec('pcm_s16le')
+        .audioFrequency(48000) // Set the correct frequency for speech recognition
+        .on('end', () => {
+          console.log('Finished converting to WAV');
+          resolve(outputPath);
+        })
+        .on('error', (err) => {
+          console.error('An error occurred during the conversion:', err);
+          reject(err);
+        })
+        .save(outputPath); // Save as WAV file
     });
+    writer.on('error', reject);
+  });
 }
-// Helper function to send an audio message to Google Cloud and get the transcription
-async function transcribeAudio(audioBuffer) {
+
+// Helper function to perform speech-to-text transcription
+async function transcribeAudio(filePath) {
+    const file = fs.readFileSync(filePath);
+    const audioBytes = file.toString('base64');
+  
     const audio = {
-      content: audioBuffer.toString('base64'),
+      content: audioBytes,
     };
   
     const config = {
       encoding: 'LINEAR16',
-      sampleRateHertz: 48000,
-      languageCode: 'en-US',
+      sampleRateHertz: 48000, // The sample rate in Hertz
+      languageCode: 'en-US', // The language of the supplied audio
     };
   
     const request = {
@@ -72,40 +66,46 @@ async function transcribeAudio(audioBuffer) {
       config: config,
     };
   
+    // Detects speech in the audio file
     const [response] = await googleClient.recognize(request);
     const transcription = response.results
-      .map((result) => result.alternatives[0].transcript)
+      .map(result => result.alternatives[0].transcript)
       .join('\n');
+  
     return transcription;
   }
-  
-  // Helper function to interact with OpenAI
-  async function askOpenAI(question) {
-    // Use the question with the OpenAI API as before
+
+
+// Update the bot.on('voice', ...) event handler
+bot.on('voice', async (ctx) => {
+  try {
+    const fileId = ctx.message.voice.file_id;
+    const fileData = await ctx.telegram.getFile(fileId);
+    const filePath = fileData.file_path;
+    const fileUrl = `https://api.telegram.org/file/bot${process.env.BOT_TOKEN}/${filePath}`;
+    const originalPath = 'voice.oga'; // Telegram voice messages are in .oga format
+    const outputPath = 'voice.wav'; // The path where the WAV file should be saved
+
+    // Call the helper function to download and convert the voice message
+    await downloadAndConvertVoiceToWAV(fileUrl, originalPath, outputPath);
+
+    // Transcribe the audio file to text
+    const transcription = await transcribeAudio(outputPath);
+    console.log('Transcription: ', transcription);
+
+    // Send the transcription back to the user
+    await ctx.reply(`Transcription: ${transcription}`);
+
+    // Cleanup: delete the local files after processing
+    fs.unlinkSync(originalPath);
+    fs.unlinkSync(outputPath);
+
+  } catch (error) {
+    console.error(error);
+    ctx.reply('I encountered an error while processing your message.');
   }
-  
-  
-  bot.on('voice', async (ctx) => {
-    try {
-      // Step 1: Get the file ID from the voice message
-      const fileId = ctx.message.voice.file_id;
-      
-      // Step 2: Get the file path from Telegram
-      const fileData = await ctx.telegram.getFile(fileId);
-      const filePath = fileData.file_path;
-      const fileUrl = `https://api.telegram.org/file/bot${process.env.BOT_TOKEN}/${filePath}`;
-      console.log(fileUrl);
-  
-      // Step 3: Download the file and convert it
-      const audioBuffer = await convertVoiceToWAV(fileUrl);
-      const transcription = await transcribeAudio(audioBuffer);
-      console.log(transcription);
-      // ... (The rest of your code for OpenAI interaction and responding to the user)
-    } catch (error) {
-      console.error(error);
-      ctx.reply('I encountered an error while processing your message.');
-    }
-  });
-  
-  // Start polling
-  bot.launch();
+});
+
+
+// Start polling
+bot.launch();
